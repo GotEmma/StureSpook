6887b0fa07c78bf9002edaec6fada13e18b5545d
Baklawa <renee.gyllensvaan@gmail.com>
2015-04-21 22:13:23 +0200

    Changed controller class from Main to ProjectController.

--- Projekt/desktop/src/edu/chl/StureSpook/desktop/DesktopLauncher.java
+++ Projekt/desktop/src/edu/chl/StureSpook/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package edu.chl.StureSpook.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import edu.chl.StureSpook.Main;
+import edu.chl.StureSpook.controller.ProjectController;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
@@ -10,6 +10,6 @@ public class DesktopLauncher {
             config.title = "StureSpook";
             config.width = 800;
             config.height = 480;
-            new LwjglApplication(new Main(), config);
+            new LwjglApplication(new ProjectController(), config);
 	}
 }
