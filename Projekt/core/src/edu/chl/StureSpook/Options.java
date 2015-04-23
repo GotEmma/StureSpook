e3415455faa2a67a748ba41d0d95441301576a32
Baklawa <renee.gyllensvaan@gmail.com>
2015-04-23 10:21:35 +0200

    Added controls with getters and setters to Options class. Made Options abstract and variables static.

--- Projekt/core/src/edu/chl/StureSpook/Options.java
+++ Projekt/core/src/edu/chl/StureSpook/Options.java
@@ -5,10 +5,29 @@
  */
 package edu.chl.StureSpook;
 
+import com.badlogic.gdx.Input;
+
 /**
  *
  * @author emmafahlen
  */
-public class Options {
+public abstract class Options {
+    
+    private static Input.Keys walkLeftKey;
+    private static Input.Keys walkRightKey;
+    private static Input.Keys jumpKey;
+    private static Input.Keys crouchKey;
+    
+    public static Input.Keys getWalkLeftKey() {return Options.walkLeftKey; }
+    public static void setWalkLeftKey(Input.Keys k) { Options.walkLeftKey = k; }
+    
+    public static Input.Keys getWalkRightKey() {return Options.walkRightKey; }
+    public static void setWalkRightKey(Input.Keys k) { Options.walkRightKey = k; }
+    
+    public static Input.Keys getJumpKey() {return Options.jumpKey; }
+    public static void setJumpKey(Input.Keys k) { Options.jumpKey = k; }
+    
+    public static Input.Keys getCrouchKey() {return Options.crouchKey; }
+    public static void setCrouchKey(Input.Keys k) { Options.crouchKey = k; }
     
 }
