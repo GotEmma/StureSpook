1303d13a6798fe9c52214044b4ee74fe7664b493
Baklawa <renee.gyllensvaan@gmail.com>
2015-04-23 18:03:45 +0200

    Forgot to add DrawableShape class.

---
+++ Projekt/core/src/edu/chl/StureSpook/model/DrawableShape.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.StureSpook.model;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+/**
+ *
+ * @author User
+ */
+public interface DrawableShape {
+    public void draw(ShapeRenderer renderer);
+}
