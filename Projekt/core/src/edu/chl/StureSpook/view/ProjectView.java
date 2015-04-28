44ee6306b25571efb338591f648f4d3b504b64cf
Baklawa <renee.gyllensvaan@gmail.com>
2015-04-28 09:07:22 +0200

    Fixed camera projection of ShapeRenderer, flashlight should move relatively correctly now.

--- Projekt/core/src/edu/chl/StureSpook/view/ProjectView.java
+++ Projekt/core/src/edu/chl/StureSpook/view/ProjectView.java
@@ -43,7 +43,7 @@ public class ProjectView implements GameView{
         renderer.setAutoShapeType(true);
         camera = new OrthographicCamera();
         camera.setToOrtho(false);
-        renderer.setProjectionMatrix(camera.combined);
+        //renderer.setProjectionMatrix(camera.combined);
         this.loadAssets();
     }
     
@@ -60,8 +60,7 @@ public class ProjectView implements GameView{
             //batch.draw(this.textures.get(i.getTextureName()), i.getX(), i.getY());
             i.draw(batch, this.textures);
         }
-        camera.update();
-        camera.position.set(model.getWorld().getPlayer().getX(), model.getWorld().getPlayer().getY(), 100);
+        
         batch.setProjectionMatrix(camera.combined);
         batch.end();
         
@@ -70,8 +69,11 @@ public class ProjectView implements GameView{
         for (DrawableShape s : shapes) {
             s.draw(renderer);
         }
+        renderer.setProjectionMatrix(camera.combined);
         renderer.end();
         
+        camera.update();
+        camera.position.set(model.getWorld().getPlayer().getX(), model.getWorld().getPlayer().getY(), 100);
     }
 
 
