1510773d828ac5b9bf3245f74b2d9880b64da0bb
Linnea Svensson <svensson.47@hotmail.com>
2015-05-06 15:22:13 +0200

    added JUnit test class for World.

--- Projekt/core/test/edu/chl/StureSpook/model/WorldTest.java
+++ Projekt/core/test/edu/chl/StureSpook/model/WorldTest.java
@@ -5,10 +5,88 @@
  */
 package edu.chl.StureSpook.model;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  *
- * @author Née
+ * @author Linneas
  */
 public class WorldTest {
+    
+    public WorldTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testSetMoveLeft() {
+    }
+
+    @Test
+    public void testSetMoveRight() {
+    }
+
+    @Test
+    public void testGetSprites() {
+    }
+
+    @Test
+    public void testGetShapes() {
+    }
+
+    @Test
+    public void testSetJump() {
+    }
+
+    @Test
+    public void testSetFlashlightPosition() {
+    }
+
+    @Test
+    public void testGetPlayer() {
+    }
+
+    @Test
+    public void testGetTiles() {
+    }
+
+    @Test
+    public void testGetFlashlightPolygon() {
+    }
+
+    @Test
+    public void testGetCurrentLevel() {
+    }
+
+    @Test
+    public void testAddPropertyChangeListener() {
+    }
+
+    @Test
+    public void testRemovePropertyChangeListener() {
+    }
     
 }
