2e84a8caf3b4a92d7f30bc32c364f9ae9cdd26e9
Baklawa <renee.gyllensvaan@gmail.com>
2015-05-06 17:27:15 +0200

    Added test for Player

---
+++ Projekt/core/test/edu/chl/StureSpook/model/PlayerTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.chl.StureSpook.model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class PlayerTest {
+    private Player player;
+    
+    public PlayerTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        player = new Player();
+    }
+    
+    @After
+    public void tearDown() {
+        player = null;
+    }
+
+    @Test
+    public void testMoveRight() {
+        float x1 = player.getX();
+        player.setMoveRight(true);
+        player.updateMotion();
+        float x2 = player.getX();
+        assertTrue(x2 > x1);
+    }
+    
+    @Test
+    public void testMoveLeft() {
+        float x1 = player.getX();
+        player.setMoveLeft(true);
+        player.updateMotion();
+        float x2 = player.getX();
+        assertTrue(x2 < x1);
+    }
+    
+    @Test
+    public void testJump() {
+        float y1 = player.getY();
+        player.setJump();
+        player.updateMotion();
+        float y2 = player.getY();
+        assertTrue(y2 > y1);
+    }
+    
+    @Test
+    public void testStandStill() {
+        float x1 = player.getX();
+        float y1 = player.getY();
+        player.updateMotion();
+        float x2 = player.getX();
+        float y2 = player.getY();
+        assertTrue(x1 == x2);
+        assertTrue(y1 == y2);
+    }
+
+    
+}
