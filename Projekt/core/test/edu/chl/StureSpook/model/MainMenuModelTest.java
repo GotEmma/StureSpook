4cf33923e73c6bcc9500245ce550083a785e1990
Baklawa <renee.gyllensvaan@gmail.com>
2015-05-31 22:40:45 +0200

    More tests.

--- Projekt/core/test/edu/chl/StureSpook/model/MainMenuModelTest.java
+++ Projekt/core/test/edu/chl/StureSpook/model/MainMenuModelTest.java
@@ -5,6 +5,8 @@
  */
 package edu.chl.StureSpook.model;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,60 +19,101 @@ import static org.junit.Assert.*;
  * @author Linneas
  */
 public class MainMenuModelTest {
+    private MainMenuModel model;
+    private TestListener listener;
     
-    public MainMenuModelTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
     
     @Before
     public void setUp() {
+        model = new MainMenuModel();
+        model.init();
+        listener = new TestListener();
+        model.addPropertyChangeListener(listener);
     }
     
     @After
     public void tearDown() {
+        model.removePropertyChangeListener(listener);
+        model = null;
+        listener = null;
     }
 
     @Test
     public void testUpdate() {
-    }
-
-    @Test
-    public void testInit() {
+        model.update();
+        assertTrue(listener.isTriggered());
+        assertEquals("logic updated", listener.getEvent().getPropertyName());
     }
 
     @Test
     public void testTrigger() {
+        model.trigger();
+        assertTrue(listener.isTriggered());
+        assertEquals("New Game", listener.getEvent().getPropertyName());
     }
 
     @Test
     public void testMoveDown() {
+        int firstIndex = model.getSelectedIndex();
+        model.moveDown();
+        assertEquals(firstIndex+1, model.getSelectedIndex());
     }
 
     @Test
     public void testMoveUp() {
+        int firstIndex = model.getSelectedIndex();
+        model.moveDown();
+        model.moveDown();
+        model.moveUp();
+        assertEquals(firstIndex+1, model.getSelectedIndex());
     }
-
+    
     @Test
-    public void testGetSelectedItem() {
+    public void testMenuLoopFirstToLast() {
+        model.moveUp();
+        int expectedIndex = model.getMenuItems().length-1;
+        assertEquals(expectedIndex, model.getSelectedIndex());
+    }
+    
+    @Test
+    public void testMenuLoopLastToFirst() { 
+        // depends on FirstToLast, make sure that one works before trying to fix this.
+        int originalIndex = model.getSelectedIndex();
+        model.moveUp();
+        model.moveDown();
+        assertEquals(originalIndex, model.getSelectedIndex());
     }
 
     @Test
     public void testGetMenuItems() {
+        //depends on moveDown
+        MenuItem[] items = model.getMenuItems();
+        for (MenuItem i : items) {
+            assertEquals(i,model.getSelectedItem());
+            model.trigger();
+            assertEquals(listener.event.getPropertyName(),i.getName());
+            model.moveDown();
+        }
     }
-
-    @Test
-    public void testAddPropertyChangeListener() {
-    }
-
-    @Test
-    public void testRemovePropertyChangeListener() {
+    
+    private class TestListener implements PropertyChangeListener {
+        private PropertyChangeEvent event;
+        private boolean triggered = false;
+        
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            this.event = evt;
+            this.triggered = true;
+        }
+        
+        public boolean isTriggered() {
+            return this.triggered;
+        }
+        
+        public PropertyChangeEvent getEvent() {
+            return this.event;
+        }
+    
     }
     
 }
